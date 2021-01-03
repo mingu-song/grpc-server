4f1b5751bf36bcb38369331e02a4601282b82596
jaden-song <seoA2016##>
2021-01-03 23:18:00 +0900

    grpc server

---
+++ src/main/java/song/mingu/grpcserver/GrpcRunner.java
@@ -0,0 +1,43 @@
+package song.mingu.grpcserver;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class GrpcRunner implements ApplicationRunner {
+    private static final int PORT = 3030;
+    private static final Server SERVER = ServerBuilder.forPort(PORT)
+            .addService(new SampleServiceImpl())
+            //.directExecutor() //nettey의 쓰레드를 이용한다. 무조건 Reactive하게 짜야함. 블록킹되면 성능최악
+            .executor(Executors.newFixedThreadPool(100))
+            .build();
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        start();
+    }
+
+    private void start() throws IOException {
+        SERVER.start();
+        log.info("GrpcRunner#start - listen port = {}", PORT);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.error("GrpcRunner#stop - Stopping gRPC server");
+            try {
+                SERVER.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.error("GrpcRunner#stop - gRPC server was stopped");
+        }));
+    }
+}
---
+++ src/main/java/song/mingu/grpcserver/SampleServiceImpl.java
@@ -0,0 +1,23 @@
+package song.mingu.grpcserver;
+
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import song.mingu.proto.SampleRequest;
+import song.mingu.proto.SampleResponse;
+import song.mingu.proto.SampleServiceGrpc;
+
+@Slf4j
+@Service
+public class SampleServiceImpl extends SampleServiceGrpc.SampleServiceImplBase {
+
+    @Override
+    public void sampleCall(SampleRequest request, StreamObserver<SampleResponse> responseObserver) {
+        log.info("SampleServiceImpl#sampleCall - {}, {}, CurrentThread = {}", request.getUserId(), request.getMessage(), Thread.currentThread().getName());
+        SampleResponse sampleResponse = SampleResponse.newBuilder()
+                .setMessage("grpc service response")
+                .build();
+        responseObserver.onNext(sampleResponse);
+        responseObserver.onCompleted();
+    }
+}
